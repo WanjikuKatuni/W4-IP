3b5b53cff51912fecc18e4791c8c261a5e0e7f9c
wanjiku <annritawanjiku77@gmail.com>
2022-03-22 02:00:00 +0300

    format

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ function pizzaOrder(size, crust, toppings, total) {
   this.toppings = toppings;
   this.total = total;
 }
-var grandTotal=0;
-var deliver=200
+var grandTotal = 0;
+var deliver = 200;
 
 $(document).ready(function () {
   // hide table
-  $(".order-table").hide()
-  $("#amount").hide()
+  $(".order-table").hide();
+  $("#amount").hide();
   // $(".order-again").hide()
 
   $("form#place-order").submit(function (event) {
@@ -19,10 +19,10 @@ $(document).ready(function () {
     event.preventDefault();
 
     // // show table after clicking placec order
-     $(".order-table").show()
-     $("button#place").hide()
-     $("button#order-again").hide()
-     $("button#restart").show()
+    $(".order-table").show();
+    $("button#place").hide();
+    $("button#order-again").hide();
+    $("button#restart").show();
 
     var selectedPizzaSize = $("#pizza-size option:selected").val();
     var sizeName = $("#pizza-size option:selected").text();
@@ -33,10 +33,12 @@ $(document).ready(function () {
     var selectedPizzaTopping = $("#pizza-topping option:selected").val();
     var topName = $("#pizza-topping option:selected").text();
 
-    var total = parseInt(selectedPizzaSize) + parseInt(selectedPizzaCrust) + parseInt(selectedPizzaTopping);
-    
-    grandTotal=grandTotal+total;
-    
+    var total =
+      parseInt(selectedPizzaSize) +
+      parseInt(selectedPizzaCrust) +
+      parseInt(selectedPizzaTopping);
+
+    grandTotal = grandTotal + total;
 
     // $("order-table").show()
 
@@ -48,101 +50,115 @@ $(document).ready(function () {
     );
 
     // insert new row
-    var newRow = '<tr><td>' + sizeName + " " + newPizzaOrder.size + '</td><td>' + crustName + " " + newPizzaOrder.crust+ '</td><td>' + topName + " " + newPizzaOrder.toppings + '</td><td>' + newPizzaOrder.total + '</td></tr>'
+    var newRow =
+      "<tr><td>" +
+      sizeName +
+      " " +
+      newPizzaOrder.size +
+      "</td><td>" +
+      crustName +
+      " " +
+      newPizzaOrder.crust +
+      "</td><td>" +
+      topName +
+      " " +
+      newPizzaOrder.toppings +
+      "</td><td>" +
+      newPizzaOrder.total +
+      "</td></tr>";
 
-      $("#pizzatable").append(newRow);
-
-    
-
+    $("#pizzatable").append(newRow);
   });
 
-  $("button#add-order").click(function(){
-      
+  $("button#add-order").click(function () {
+    var selectedPizzaSize = $("#pizza-size option:selected").val();
+    var sizeName = $("#pizza-size option:selected").text();
 
-      var selectedPizzaSize = $("#pizza-size option:selected").val();
-      var sizeName = $("#pizza-size option:selected").text();
-  
-      var selectedPizzaCrust = $("#pizza-crust option:selected").val();
-      var crustName = $("#pizza-crust option:selected").text();
-  
-      var selectedPizzaTopping = $("#pizza-topping option:selected").val();
-      var topName = $("#pizza-topping option:selected").text();
-  
-      var total = parseInt(selectedPizzaSize) + parseInt(selectedPizzaCrust) + parseInt(selectedPizzaTopping);
-      grandTotal=grandTotal+total;
-     
+    var selectedPizzaCrust = $("#pizza-crust option:selected").val();
+    var crustName = $("#pizza-crust option:selected").text();
 
-      // $("order-table").show()
-  
-      var newPizzaOrder = new pizzaOrder(
-        selectedPizzaSize,
-        selectedPizzaCrust,
-        selectedPizzaTopping,
-        total
-      );
-  
-      // insert new row
-      var newRow = '<tr><td>' + sizeName + " " + newPizzaOrder.size + '</td><td>' + crustName + " " + newPizzaOrder.crust+ '</td><td>' + topName + " " + newPizzaOrder.toppings + '</td><td>' + newPizzaOrder.total + '</td></tr>'
-  
-        $("#pizzatable").append(newRow);
+    var selectedPizzaTopping = $("#pizza-topping option:selected").val();
+    var topName = $("#pizza-topping option:selected").text();
 
+    var total =
+      parseInt(selectedPizzaSize) +
+      parseInt(selectedPizzaCrust) +
+      parseInt(selectedPizzaTopping);
+    grandTotal = grandTotal + total;
 
-  
-  })
+    // $("order-table").show()
 
-  $("button#finish-order").click(function(){
-      $("#amount").show()
-      $("button#order-again").hide()
-      $(".pizzatable").hide()
-      $("button#place").hide()
-      $("button#add-order").hide()
-      $("button#finish-order").hide()
-      $("#dtotal").show()
-      $("#totalamount").hide()
-      $("form#location").hide()
-      $("#pay").show()
-      $("button#restart").show()
-      
-      $(".grand-total").text(grandTotal);
-  })
+    var newPizzaOrder = new pizzaOrder(
+      selectedPizzaSize,
+      selectedPizzaCrust,
+      selectedPizzaTopping,
+      total
+    );
 
-  $("#delivery").click (function(){
-    
-      $("#totalamount").hide()
-      $("form#location").show()
-      $("#delivery").hide()
-      $("#deliveryHelp").hide()
-      $("#pay").hide()
-      $("#dtotal").hide()
+    // insert new row
+    var newRow =
+      "<tr><td>" +
+      sizeName +
+      " " +
+      newPizzaOrder.size +
+      "</td><td>" +
+      crustName +
+      " " +
+      newPizzaOrder.crust +
+      "</td><td>" +
+      topName +
+      " " +
+      newPizzaOrder.toppings +
+      "</td><td>" +
+      newPizzaOrder.total +
+      "</td></tr>";
 
-   
-  })
+    $("#pizzatable").append(newRow);
+  });
 
-  $("form#location").submit(function(event){
-      //prevent default action
+  $("button#finish-order").click(function () {
+    $("#amount").show();
+    $("button#order-again").hide();
+    $(".pizzatable").hide();
+    $("button#place").hide();
+    $("button#add-order").hide();
+    $("button#finish-order").hide();
+    $("#dtotal").show();
+    $("#totalamount").hide();
+    $("form#location").hide();
+    $("#pay").show();
+    $("button#restart").show();
+
+    $(".grand-total").text(grandTotal);
+  });
+
+  $("#delivery").click(function () {
+    $("#totalamount").hide();
+    $("form#location").show();
+    $("#delivery").hide();
+    $("#deliveryHelp").hide();
+    $("#pay").hide();
+    $("#dtotal").hide();
+  });
+
+  $("form#location").submit(function (event) {
+    //prevent default action
     event.preventDefault();
 
-
     var inputtednewLocation = $("input#new-location").val();
-    $(".the-location").text(inputtednewLocation)
-    grandTotal=grandTotal+deliver
+    $(".the-location").text(inputtednewLocation);
+    grandTotal = grandTotal + deliver;
     $(".delivery-total").text(grandTotal);
 
-
-    $("#dtotal").hide()
-    $("#delivery-switch").hide()
-    $("#totalamount").show()
-    $("form#location").hide()
-    $("button#place").hide()
-    $("#pay").show()
-
-    
-    
-  })
-//to reload page once done
-  $("button#restart").click(function(){
+    $("#dtotal").hide();
+    $("#delivery-switch").hide();
+    $("#totalamount").show();
+    $("form#location").hide();
+    $("button#place").hide();
+    $("#pay").show();
+  });
+  //to reload page once done
+  $("button#restart").click(function () {
     location.reload();
-})
- 
-})
-
+  });
+});

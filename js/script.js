49a7434cbb35e429944b41cf93a559fe955dda14
wanjiku <annritawanjiku77@gmail.com>
2022-03-21 10:51:35 +0300

    hide order again button

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
     // hide table
     $("#table1").hide()
     $(".amount").hide()
+    $(".order-again").hide()
     
-    
+    //show table after placing order 
+    $("button#place-order").click(function(){
+        $("#table1").show()
+    })
 })
